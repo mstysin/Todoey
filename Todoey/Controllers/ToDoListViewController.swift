66053ab55f442012957ace6f22544ef0c3a8356b
mstysin <mstysin@gmail.com>
2018-01-28 19:36:23 +0300

    Persisting data using Core Data

--- Todoey/Controllers/ToDoListViewController.swift
+++ Todoey/Controllers/ToDoListViewController.swift
@@ -7,11 +7,21 @@
 //
 
 import UIKit
+import CoreData
 
 class ToDoListViewController: UITableViewController {
     
     var itemArray = [Item]()
-    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
+    
+    //to pass category selected on the first screen
+    var selectedCategory : Category? {
+        didSet{
+            //this is being executed as soon as selectedCategory receives the value
+            loadItems()
+        }
+    }
+    
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
 
     // defaults allow to store simple data
     //let defaults = UserDefaults.standard
@@ -19,6 +29,7 @@ class ToDoListViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
         print("here you go, \(dataFilePath)")
                 
         //checking if there is saved user array and extracting it
@@ -26,7 +37,8 @@ class ToDoListViewController: UITableViewController {
         //    itemArray = items
         //}
         
-        loadItems()
+        //loaditems below is not needed anymore cause it's being triggered inside selected category above
+        //loadItems()
     }
     
     //MARK - Tableview Datasource Methods
@@ -49,7 +61,11 @@ class ToDoListViewController: UITableViewController {
         //print("you selected row \(itemArray[indexPath.row])")
         
         //changing false to ture and otherwise for checkmark attribute
-        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        
+         itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+//        context.delete(itemArray[indexPath.row])
+//        itemArray.remove(at: indexPath.row)
+        
         saveItems()
         
         //commented reload below cause it's already applied in saveItems above
@@ -67,9 +83,12 @@ class ToDoListViewController: UITableViewController {
         
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             //what will happen once the user clicks the add item button on UIalert
-            let createdItem = Item()
-            createdItem.title = textField.text!
-            self.itemArray.append(createdItem)
+            
+            let newItem = Item(context: self.context)
+            newItem.title = textField.text!
+            newItem.done = false
+            newItem.parentCategory = self.selectedCategory
+            self.itemArray.append(newItem)
             
             //saving array with appended items to local storage on the phone so upon reload user sees he's added items
             //self.defaults.set(self.itemArray, forKey: "ToDoListArray")
@@ -89,29 +108,81 @@ class ToDoListViewController: UITableViewController {
     }
     
     func saveItems() {
-        let encoder = PropertyListEncoder()
+
         do {
-            let data = try encoder.encode(itemArray)
-            try data.write(to: dataFilePath!)
+            try context.save()
         } catch {
-            print("Error encoding item array, \(error)")
+            print("Error saving context, \(error)")
         }
-        
         
         //reloading the tableview to show added item
         self.tableView.reloadData()
     }
     
-    func loadItems() {
-        if let data = try? Data(contentsOf: dataFilePath!) {
-            let decoder = PropertyListDecoder()
-            do {
-                itemArray = try decoder.decode([Item].self, from: data)
-            } catch {
-                print("Eror decoding item array, \(error)")
+    //function that loads items from data table.
+    //It uses default (= Item.fetchRequest()) that extracts all available data if custom request is not provided
+    func loadItems(with request: NSFetchRequest<Item> = Item.fetchRequest(), predicate: NSPredicate? = nil) {
+        
+        let categoryPredicate = NSPredicate(format: "parentCategory.name MATCHES %@", selectedCategory!.name!)
+
+        if let additionalPredicate = predicate {
+            request.predicate = NSCompoundPredicate(andPredicateWithSubpredicates: [categoryPredicate, additionalPredicate])
+        } else {
+            request.predicate = categoryPredicate
+            
+        }
+        
+//        let compoundPredicate = NSCompoundPredicate(andPredicateWithSubpredicates: [categoryPredicate, predicate])
+//        request.predicate = compoundPredicate
+        
+        // let request : NSFetchRequest<Item> = Item.fetchRequest()
+
+        do {
+            itemArray = try context.fetch(request)
+        } catch {
+            print("Error fetching data from context, \(error)")
+        }
+        
+        tableView.reloadData()
+    }
+    
+}
+
+//MARK: - searcj bar methods
+extension ToDoListViewController: UISearchBarDelegate  {
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        let request : NSFetchRequest<Item> = Item.fetchRequest()
+        
+        let predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)
+        
+        request.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
+        
+        loadItems(with: request, predicate: predicate)
+        
+//        do {
+//            itemArray = try context.fetch(request)
+//        } catch {
+//            print("Error fetching data from context, \(error)")
+//        }
+        
+        // tableView.reloadData()
+        // print(searchBar.text!)
+    }
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        loadItems()
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchBar.text?.count == 0 {
+            loadItems()
+            
+            DispatchQueue.main.async {
+                searchBar.resignFirstResponder()
+                //makes search bar out of focus and no blinking type writer
             }
         }
     }
     
 }
-
